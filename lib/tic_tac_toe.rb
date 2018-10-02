af1597551faaa18613c59af70c025e3adf5239c6
Andrew Bernier <a6bernier@gmail.com>
2018-10-02 21:18:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,7 +122,7 @@ def winner(board)
 end
 
 def play(board)
-  until over?(board) == true
+  while over?(board) == true do
     turn(board)
     if winner(board) == "X"
       puts "Congratulations X!"
